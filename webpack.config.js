6e91b409870b07b3f90cdf7b9b195025edda84da
Andrei Wilkens | Paints <a.wilkens@paints.de>
2016-07-01 17:11:08 +0200

    [INIT]

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
+
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
